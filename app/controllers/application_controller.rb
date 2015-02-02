48e6e23eebaf774f53301b1bf7ab1d4abf00bfe9
Orhan Can Ceylan <can.ceylan@derivalabs.com>
2015-02-02 22:30:31 +0200

    domain tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :check_domain
   protect_from_forgery with: :null_session
   skip_before_filter :verify_authenticity_token
   after_filter :cors_set_access_control_headers
 
   include Authenticable
+
+  def check_domain
+    if Rails.env.production? and request.host.downcase != 'deriva.co'
+      redirect_to request.protocol + 'deriva.co' + request.fullpath, :status => 301
+    end
+  end
 
   def cors_set_access_control_headers
   	headers['Access-Control-Allow-Origin'] = *
